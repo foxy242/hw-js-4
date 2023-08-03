f092855e368113602b2884c979a6e7802572f335
foxy242 <114094487+foxy242@users.noreply.github.com>
2023-08-03 14:23:00 +0300

    6

--- nay.js
+++ nay.js
@@ -1,45 +1,48 @@
-// завданя 1
+// // завданя 1
 
-console.log(Number.parseInt("5px"));
-console.log(Number.parseInt("12qwe74"));
-console.log(Number.parseInt("12.46qwe79"));
-console.log(Number.parseInt("qweqwe"));
+// console.log(Number.parseInt("5px"));
+// console.log(Number.parseInt("12qwe74"));
+// console.log(Number.parseInt("12.46qwe79"));
+// console.log(Number.parseInt("qweqwe"));
 
-const к1 = 5
+// const к1 = 5
 
-const к2 = 12
+// const к2 = 12
 
-const к3 = 12
+// const к3 = 12
 
-const к4 = NaN
+// const к4 = NaN
 
-// завданя 2
+// // завданя 2
 
-console.log(Number.parseFloat("5px"));
-console.log(Number.parseFloat("12qwe74"));
-console.log(Number.parseFloat("12.46qwe79"));
-console.log(Number.parseFloat("qweqwe")); 
+// console.log(Number.parseFloat("5px"));
+// console.log(Number.parseFloat("12qwe74"));
+// console.log(Number.parseFloat("12.46qwe79"));
+// console.log(Number.parseFloat("qweqwe")); 
 
-const x1 = 5
+// const x1 = 5
 
-const x2 = 12
+// const x2 = 12
 
-const x3 = 12.46
+// const x3 = 12.46
 
-const x4 = NaN
+// const x4 = NaN
 
-// завданя 3
+// // завданя 3
 
-console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
 
-console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
 
-// завданя 4
+// // завданя 4
 
-console.log(Math.random() * (19 - 3) + 1);
+// console.log(Math.random() * (19 - 3) + 1);
 
-// завданя 5
+// // завданя 5
 
-
+// const corin = 24
+// console.log(Math.sqrt(24))
 
 // завданя 6
+
+console.log(parseInt('123'));
